1035b2203983d624b9fd8789f54f3f4f8b83417f
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2024-12-29 03:02:11 +1300

    adding tech stack to footer

--- clen3t-web/src/components/Footer.jsx
+++ clen3t-web/src/components/Footer.jsx
@@ -1,9 +1,17 @@
+import { BsQuestionSquareFill } from "react-icons/bs";
+
 const Footer = () => {
 
-    
+    const handleClick = () => {
+        alert("Tech stack: ReactJS, Java Spring Boot, MongoDB")
+    }
+
     return ( 
-        <div className="ml-[1%]">
+        <div className="mx-[1%] display flex justify-between items-end">
             <p>github.com/clen678</p>
+            <button className="hover:text-primary-blue active:text-primary-blue-darker" onClick={handleClick}>
+                <BsQuestionSquareFill size={20} />
+            </button>
         </div>
      );
 }
