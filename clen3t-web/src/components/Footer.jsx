d924f557bb24971fd791e491cfbe32bf94717bec
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-01-19 21:59:21 +1300

    adjusting information button

--- clen3t-web/src/components/Footer.jsx
+++ clen3t-web/src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import { BsQuestionSquareFill } from "react-icons/bs";
+import { IoInformationCircle } from "react-icons/io5";
 
 const Footer = () => {
 
     const handleClick = () => {
-        alert("Tech stack: ReactJS, Java Spring Boot, MongoDB")
+        alert("Tech stack: \nReactJS,\nSpring Boot,\nMongoDB,\nTailwind CSS\n\nJava,\nJavascript,\nHTML/CSS")
     }
 
     return ( 
         <div className="mx-[1%] display flex justify-between items-end w-full mt-auto py-2 px-4">
             <p>github.com/clen678</p>
             <button className="hover:text-primary-blue active:text-primary-blue-darker" onClick={handleClick}>
-                <BsQuestionSquareFill size={20} />
+                <IoInformationCircle size={22} />
             </button>
         </div>
      );
