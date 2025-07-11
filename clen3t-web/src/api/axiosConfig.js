cea9c15177f0ec8b71397c2cdbf18ad704c12fdb
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-07-12 01:26:32 +1200

    backend configuration for deployment on fly.io

--- clen3t-web/src/api/axiosConfig.js
+++ clen3t-web/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080', //http://localhost:8080
+    baseURL:'https://clen3t-white-fog-5060.fly.dev', //http://localhost:8080
     headers:{"ngrok-skip-browser-warning" : "true"}
 });
