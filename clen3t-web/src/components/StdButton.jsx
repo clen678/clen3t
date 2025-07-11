8e8cc96262fd51ca156ebb8a3c5b7ec7da7e93b7
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-07-12 04:18:54 +1200

    adding dockerfiles and disabling button when sending

--- clen3t-web/src/components/StdButton.jsx
+++ clen3t-web/src/components/StdButton.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 
-const StdButton = ({text, link, colour, onClick}) => {
+const StdButton = ({text, link, colour, onClick, disabled}) => {
 
 
     return ( 
         <Link to={link}
-            className={`flex items-center text-xl py-1 px-6 rounded-lg w-[7.5rem] justify-center max-md:text-lg max-md:w-[5.6rem] max-md:px-3 ${colour === "blue"
+            className={`flex items-center text-xl py-1 px-6 rounded-lg w-[7.5rem] justify-center max-md:text-lg max-md:w-[5.6rem] max-md:px-3
+            ${colour === "blue"
             ? "bg-primary-blue hover:bg-primary-blue-dark active:bg-primary-blue-darker"
-            : "bg-primary-red hover:bg-primary-red-dark active:bg-primary-red-darker"
-        }`}
-        onClick={onClick}>{text}</Link>
+            : "bg-primary-red hover:bg-primary-red-dark active:bg-primary-red-darker"}
+            ${disabled ? (colour === "blue" ? "bg-primary-blue-dark cursor-default" : "bg-primary-red-dark cursor-default") : ""}`}
+            onClick={disabled ? null : onClick}>
+            {text}
+        </Link>
      );
 }
  
